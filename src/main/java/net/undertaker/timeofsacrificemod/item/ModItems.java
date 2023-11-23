ebc18eb7c6806902d12fa13d50391ea66748ceba
undertakerJ <justghost1242@gmail.com>
2023-11-23 19:49:16 +0200

    added "stimulator" item

--- src/main/java/net/undertaker/timeofsacrificemod/item/ModItems.java
+++ src/main/java/net/undertaker/timeofsacrificemod/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.undertaker.timeofsacrificemod.TimeOfSacrifice;
+import net.undertaker.timeofsacrificemod.item.custom.StimulatorItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -14,7 +15,9 @@ public class ModItems {
     public static final RegistryObject<Item> PLATINUM_INGOT = ITEMS.register("platinum_ingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB)));
     public static final RegistryObject<Item> PLATINUM_ORE = ITEMS.register("raw_platinum_ore",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB)));
+            () -> new StimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB)));
+ public static final RegistryObject<Item> STIMULATOR = ITEMS.register("stimulator",
+            () -> new StimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
