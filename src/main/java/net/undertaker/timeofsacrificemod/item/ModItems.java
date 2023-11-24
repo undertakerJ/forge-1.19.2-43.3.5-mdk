99216647add5b78a4ed6a579a99b023b9eddacf3
undertakerJ <justghost1242@gmail.com>
2023-11-24 22:56:38 +0200

    Added "Dragon Slayer Sword"

--- src/main/java/net/undertaker/timeofsacrificemod/item/ModItems.java
+++ src/main/java/net/undertaker/timeofsacrificemod/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.undertaker.timeofsacrificemod.item;
 
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,16 +17,25 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB)));
     public static final RegistryObject<Item> PLATINUM_ORE = ITEMS.register("raw_platinum_ore",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB)));
- public static final RegistryObject<Item> SPEED_STIMULATOR = ITEMS.register("speed_stimulator",
+    public static final RegistryObject<Item> SPEED_STIMULATOR = ITEMS.register("speed_stimulator",
             () -> new SpeedStimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB).stacksTo(1)));
-public static final RegistryObject<Item> REGENERATION_STIMULATOR = ITEMS.register("regeneration_stimulator",
+    public static final RegistryObject<Item> REGENERATION_STIMULATOR = ITEMS.register("regeneration_stimulator",
             () -> new RegenerationStimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB).stacksTo(1)));
-public static final RegistryObject<Item> STRENGTH_STIMULATOR = ITEMS.register("strength_stimulator",
+    public static final RegistryObject<Item> STRENGTH_STIMULATOR = ITEMS.register("strength_stimulator",
             () -> new StrengthStimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB).stacksTo(1)));
-public static final RegistryObject<Item> ULTIMATE_STIMULATOR = ITEMS.register("ultimate_stimulator",
+    public static final RegistryObject<Item> ULTIMATE_STIMULATOR = ITEMS.register("ultimate_stimulator",
             () -> new UltimateStimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB).stacksTo(1)));
-public static final RegistryObject<Item> EXPERIMENTAL_STIMULATOR = ITEMS.register("experimental_stimulator",
+    public static final RegistryObject<Item> EXPERIMENTAL_STIMULATOR = ITEMS.register("experimental_stimulator",
             () -> new ExperimentalStimulatorItem(new Item.Properties().tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB).stacksTo(1)));
+    public static final RegistryObject<Item> DRAGON_SLAYER_SWORD = ITEMS.register("dragon_slayer_sword",
+            () -> {
+                return new SwordItem(Tiers.NETHERITE, 20, -3.5f,
+                        new Item.Properties()
+                                .tab(ModCreativeModeTab.TIME_OF_SACRIFICE_TAB)
+                                .stacksTo(1)
+                                .defaultDurability(2000)
+                                .rarity(Rarity.EPIC));
+            });
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
