bde3fe832a1ade5b1cf684a98d34e6b179468223
undertakerJ <justghost1242@gmail.com>
2023-11-27 01:16:53 +0200

    Rework of Amethyst Dagger: Now teleports behind entity you looking at Applies Invisibility for 3 seconds, and Guaranteed Crit Effect
    New Guaranteed Crit Effect - applies 100% crit chance for 1 hit.

--- src/main/java/net/undertaker/timeofsacrificemod/item/custom/AmethystDaggerItem.java
+++ src/main/java/net/undertaker/timeofsacrificemod/item/custom/AmethystDaggerItem.java
@@ -10,7 +10,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -19,7 +18,6 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.undertaker.timeofsacrificemod.TimeOfSacrifice;
@@ -45,7 +43,6 @@ public class AmethystDaggerItem extends SwordItem {
         return ProjectileUtil.getEntityHitResult(
                 player.level, player, startPos, endPos, searchArea, filter);
     }
-
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
         if (entity instanceof LivingEntity livingEntity) {
@@ -101,11 +98,8 @@ public class AmethystDaggerItem extends SwordItem {
                         player.getItemInHand(interactionHand).hurtAndBreak(8, player, p -> p.broadcastBreakEvent(InteractionHand.MAIN_HAND));
                     }
                 }
-
             }
-
         }
-
         return super.use(level, player, interactionHand);
     }
 
